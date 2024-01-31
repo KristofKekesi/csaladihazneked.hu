cc350317454118c07ccbdd606d49d64e8710bd40
Kristóf Kékesi <64812024+KristofKekesi@users.noreply.github.com>
2024-01-31 21:41:43 +0100

    Update page.tsx

--- www/src/app/blog/[id]/page.tsx
+++ www/src/app/blog/[id]/page.tsx
@@ -12,11 +12,12 @@ import PostSmall from "@/components/blog/PostSmall";
 
 
 type Props = {
-	params: { id: string }
+	params: { id: string },
+	searchParams: { [key: string]: string | string[] | undefined }
   }
    
   export async function generateMetadata(
-	{ params }: Props,
+	{ params}: Props,
 	parent: ResolvingMetadata
   ): Promise<Metadata> {
 	const post: Post = await getRandomPost();
