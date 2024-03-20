2286d73144f1b1a758aff8fbb67258b91ea9a934
Kristóf Kékesi <64812024+KristofKekesi@users.noreply.github.com>
2024-03-20 12:30:27 +0100

    feat: Add custom next and previous carousel navigation buttons
    Added custom next and previous carousel navigation buttons with functionality to scroll to the next and previous items in the carousel. This improves the user experience by providing more intuitive navigation options.

---
+++ www/src/components/general/ImageCarouselNavigation.tsx
@@ -0,0 +1,59 @@
+import { ChevronLeft, ChevronRight } from "lucide-react";
+import { Button } from "../ui/Button";
+import { cn } from "@/lib/utils";
+import { useCarousel } from "../ui/Carousel";
+
+/**
+ * A custom next item component.
+ * @param params Basic `HTMLButtonElement` type. 
+ * @returns A custom next item component.
+ */
+export function CustomCarouselNext(params: Partial<HTMLButtonElement>) {
+// eslint-disable-next-line no-unused-vars
+const { orientation, scrollNext, canScrollNext } = useCarousel();
+
+  return (
+    <Button
+      //ref={ref}
+      variant="outline"
+      size="icon"
+      className={cn(
+        "size-8",
+        params.className
+      )}
+      disabled={!canScrollNext}
+      onClick={scrollNext}
+
+    >
+      <ChevronRight className="size-4" />
+      <span className="sr-only">Következő</span>
+    </Button>
+  );
+}
+/**
+ * A custom next item component.
+ * @param params Basic `HTMLButtonElement` type. 
+ * @returns A custom next item component.
+ */
+export function CustomCarouselPrev(params: Partial<HTMLButtonElement>) {
+	// eslint-disable-next-line no-unused-vars
+	const { orientation, scrollPrev, canScrollPrev } = useCarousel();
+
+	return (
+		<Button
+			//ref={ref}
+			variant="outline"
+			size="icon"
+			className={cn(
+				"size-8",
+				params.className
+			)}
+			disabled={!canScrollPrev}
+			onClick={scrollPrev}
+
+		>
+			<ChevronLeft className="size-4" />
+			<span className="sr-only">Előző</span>
+		</Button>
+	);
+}
